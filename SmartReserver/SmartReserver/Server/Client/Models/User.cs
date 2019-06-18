e3ef963c1514db3a6f90c63907870277373f2215
percova <viktoriyacap@gmail.com>
2019-06-18 20:05:42 +0300

    Update User.cs

--- SmartReserver/SmartReserver/Server/Client/Models/User.cs
+++ SmartReserver/SmartReserver/Server/Client/Models/User.cs
@@ -6,5 +6,18 @@ namespace SmartReserver.Server.Client.Models
 {
     class User
     {
+        public string Id { get; set; }
+
+        public string Token { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Email { get; set; }
+
+        public string AvatarUrl { get; set; }
+
+        public string PhoneNumber { get; set; }
     }
 }
