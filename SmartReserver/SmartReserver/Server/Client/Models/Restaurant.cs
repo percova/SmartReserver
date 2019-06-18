d341c40b6f66a54bf3229c06077ca4acac297a5e
percova <viktoriyacap@gmail.com>
2019-06-18 20:05:13 +0300

    Update Restaurant.cs

--- SmartReserver/SmartReserver/Server/Client/Models/Restaurant.cs
+++ SmartReserver/SmartReserver/Server/Client/Models/Restaurant.cs
@@ -6,5 +6,18 @@ namespace SmartReserver.Server.Client.Models
 {
     class Restaurant
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string City { get; set; }
+        public int Rating { get; set; }
+        public int AveragePrice { get; set; }
+        public string DefaultPicture { get; set; }
+        public string Street { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+        public string CheckInTime { get; set; }
+        public string CheckOutTime { get; set; }
+        public List<string> Pictures { get; set; }
     }
 }
