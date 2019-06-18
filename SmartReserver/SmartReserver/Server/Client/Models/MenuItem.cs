941fde79ed9bc424d38ea1237626830b2cb400e0
percova <viktoriyacap@gmail.com>
2019-06-18 20:04:31 +0300

    Update MenuItem.cs

--- SmartReserver/SmartReserver/Server/Client/Models/MenuItem.cs
+++ SmartReserver/SmartReserver/Server/Client/Models/MenuItem.cs
@@ -4,7 +4,57 @@ using System.Text;
 
 namespace SmartReserver.Server.Client.Models
 {
-    class MenuItem
+    class MenuItem : BindableObject
     {
+        string title;
+        MenuItemType menuItemType;
+        Type viewModelType;
+        bool isEnabled;
+
+        public string Title
+        {
+            get => title;
+
+            set
+            {
+                title = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public MenuItemType MenuItemType
+        {
+            get => menuItemType;
+
+            set
+            {
+                menuItemType = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public Type ViewModelType
+        {
+            get => viewModelType;
+
+            set
+            {
+                viewModelType = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public bool IsEnabled
+        {
+            get => isEnabled;
+
+            set
+            {
+                isEnabled = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public Func<Task> AfterNavigationAction { get; set; }
     }
 }
