c1e3c923c49305dad69e56fb2d30c7496ac12f74
percova <viktoriyacap@gmail.com>
2019-06-18 19:23:01 +0300

    Add files via upload

---
+++ SmartReserver/SmartReserver/Server/Client/Models/Booking.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SmartReserver.Server.Client.Models
+{
+    class Booking
+    {
+        public int Id { get; set; }
+        public int RestaurantId { get; set; }
+        public string UserId { get; set; }
+        public string PhoneNumber { get; set; }
+        public int Visitors { get; set; }
+        public DateTime Time { get; set; }
+    }
+}
